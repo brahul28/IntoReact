fb905b6d2e573bcdc88dfac2283f4b1232697535
Rahul Bisht <bishtsixstring28@.com>
2026-03-03 17:44:12 +0530

    Add functional component in React

--- App.js
+++ App.js
@@ -11,6 +11,31 @@ const jsxHeading = (
   </h1>
 );
 
+//React functional component
+
+const title = (
+  <h1 id="title" className="title">
+    Title of the Project
+  </h1>
+);
+
+const Title = () => (
+  <h3 id="heading" tabIndex={6} className="title">
+    Another Title heading
+  </h3>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    {Title()}
+    <h2 id="heading" className="head">
+      This is functional component
+    </h2>
+    <Title/>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
