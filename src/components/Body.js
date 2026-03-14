ec70105f07939977982284d75526897cf59a9cd6
Rahul Bisht <bishtsixstring28@.com>
2026-03-14 21:22:38 +0530

    Add restructure of the project

---
+++ src/components/Body.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            ); 
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurant.map((restaurant, index) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
