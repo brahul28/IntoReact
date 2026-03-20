44a46b27928a3ac403db6d84ba9c8c1b4f4c1015
Rahul Bisht <bishtsixstring28@.com>
2026-03-20 17:37:55 +0530

    Add React Hooks useState and useEffect and fetch data from swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
+//import resList from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
-  return (
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING",
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+  };
+  //conditional rendering
+  // if(listOfRestaurant.length === 0){
+  //   return <Shimmer/>;
+  // }
+
+  return listOfRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurant.filter(
-              (res) => res.info.avgRating > 4
-            ); 
+              (res) => res.info.avgRating > 4.5,
+            );
             setListOfRestaurant(filteredList);
           }}
         >
